40323e1bba574ff9e1522390f0d7d6cb28f6c4c7
hmtmcse <hmtm.cse@gmail.com>
2021-09-27 18:28:23 +0600

    Fix sample code bug

--- sample/example_module/example/model/contact.py
+++ sample/example_module/example/model/contact.py
@@ -2,7 +2,7 @@ from pf_sqlalchemy.db.orm import Base, database
 
 
 class Contact(Base):
-    name = database.Column("name", database.String())
+    name = database.Column("name", database.String(100))
     email = database.Column("email", database.String(100))
     username = database.Column("username", database.String(100))
     password = database.Column("password", database.String(150))
