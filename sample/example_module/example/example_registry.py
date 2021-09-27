ab94bbfde5736856ac3cfedae28b324f0613b675
hmtmcse <hmtm.cse@gmail.com>
2021-09-27 14:38:10 +0600

    example module CRUD codes

--- sample/example_module/example/example_registry.py
+++ sample/example_module/example/example_registry.py
@@ -1,11 +1,14 @@
+from example.model.contact import database as contact_model
+from example.controller.contact_controller import contact_controller
+
 
 class ExampleRegistry:
 
     def register_model(self, flask_app):
-        pass
+        contact_model.create_all()
 
     def register_controller(self, flask_app):
-        pass
+        flask_app.register_blueprint(contact_controller)
 
 
 example_registry = ExampleRegistry()
