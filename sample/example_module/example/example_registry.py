06b17e10bd111c010605395ea68605c94a52f316
hmtmcse <hmtm.cse@gmail.com>
2021-09-27 12:46:21 +0600

    example module codes

---
+++ sample/example_module/example/example_registry.py
@@ -0,0 +1,11 @@
+
+class ExampleRegistry:
+
+    def register_model(self, flask_app):
+        pass
+
+    def register_controller(self, flask_app):
+        pass
+
+
+example_registry = ExampleRegistry()
