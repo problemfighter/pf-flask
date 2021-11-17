b9c4c56957a6abdc4851ea1861ab8b0ab30963dd
hmtmcse <hmtm.cse@gmail.com>
2021-11-17 13:21:34 +0600

    Added Setup File for Root Project and Updated the Dev Preparation Script

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pf-flask',
+    version='0.0.1',
+    url='https://github.com/problemfighter/pf-flask',
+    license='Problem Fighter License',
+    author='H.M.Touhid Mia',
+    author_email='codegen@problemfighter.com',
+    description='',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
--- tools/prepare-dev.sh
+++ tools/prepare-dev.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
+IS_IGNORE_VENV="$1"
+if [ -z "$IS_IGNORE_VENV" ]; then
+  echo "======================================================================"
+  echo "Connecting to Virtual Environment......."
+  echo "======================================================================"
 
-GIT_PATH="$(which git)"
-export git_path="$GIT_PATH"
-WINDOWS_VENV="venv/Scripts/activate"
-LINUX_VENV="venv/bin/activate"
-VENV=""
-if [ -f "$WINDOWS_VENV" ]; then
+  GIT_PATH="$(which git)"
+  export git_path="$GIT_PATH"
+  WINDOWS_VENV="venv/Scripts/activate"
+  LINUX_VENV="venv/bin/activate"
+  VENV=""
+  if [ -f "$WINDOWS_VENV" ]; then
   VENV="$WINDOWS_VENV"
-elif [ -f "$LINUX_VENV" ]; then
+  elif [ -f "$LINUX_VENV" ]; then
   VENV="$LINUX_VENV"
-fi
+  fi
 
-if [ -z "$VENV" ]; then
+  if [ -z "$VENV" ]; then
     echo "======================================================================"
     echo "Sorry The Virtual Environment not found"
     echo "======================================================================"
     exit 1
+  fi
+  source "$VENV"
 fi
 
-source "$VENV"
+
+echo "Calling Development Library Clone and Pull Script"
+echo "-------------------------------------------------------------------------------"
 python tools/dev-clone-and-pull.py
+
+echo "Installing Development Dependencies"
+echo "-------------------------------------------------------------------------------"
 pip install -r dev-requirements.txt
-deactivate
+
+
+if [ -z "$IS_IGNORE_VENV" ]; then
+  deactivate
+fi
